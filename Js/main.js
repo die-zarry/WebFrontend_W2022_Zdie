1ebda946ed8ab25812d83deb06462d1c0b1c0b90
Zarry <zarrydie03@gmail.com>
2022-01-15 22:09:27 -0500

    adding js file

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,38 @@
-function JS(){
-    const links = [
-        {
-          label: "Week1 notes",
-          url: "week1/index.html"
-        }
-      ]
+// function JS(){
+//     const links = [
+//         {
+//           label: "Week1 notes", url: "week1/index.html"
+//         }
+//       ]
     
-}
+// }
+const links = [
+
+    { label: 'Week 1 Notes', url: 'week1.html', }, // you can add other tags if you like
+  
+    { label: 'Week 2 Notes', url: 'week2.html', },
+    { label: 'Week 3 Notes', url: 'week3.html', },
+  
+  ];
+  
+   
+  
+  const ol = document.getElementById('assignments');      // whatever your OL tag ID is
+  
+  links.forEach(link => {
+  
+    let li = document.createElement('li');
+   
+    let a = document.createElement('a');
+  
+    a.setAttribute('href', link.url);
+  
+    a.innerText = link.label;
+  
+    // add other tags of interest if you like (date submitted, score)
+  
+    li.appendChild(a);
+  
+    ol.appendChild(li);
+  
+  });
