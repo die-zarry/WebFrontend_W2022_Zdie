99a6d7441fbe1e3846057c8596f1f66707cea142
Zarry <zarrydie03@gmail.com>
2022-02-13 07:18:26 -0500

    update js folder

--- Js/main.js
+++ Js/main.js
@@ -4,35 +4,59 @@
 //           label: "Week1 notes", url: "week1/index.html"
 //         }
 //       ]
-    
+
 // }
 const links = [
 
-    { label: 'Week 1 Notes', url: 'week1.html', }, // you can add other tags if you like
-  
-    { label: 'Week 2 Notes', url: 'week2.html', },
-    { label: 'Week 3 Notes', url: 'week3.html', },
-  
-  ];
-  
-   
-  
-  const ol = document.getElementById('assignments');      // whatever your OL tag ID is
-  
-  links.forEach(link => {
-  
-    let li = document.createElement('li');
-   
-    let a = document.createElement('a');
-  
-    a.setAttribute('href', link.url);
-  
-    a.innerText = link.label;
-  
-    // add other tags of interest if you like (date submitted, score)
-  
-    li.appendChild(a);
-  
-    ol.appendChild(li);
-  
-  });
+  {
+    label: 'Week1_Notes',
+    url: 'week1.html',
+  }, // you can add other tags if you like
+
+  {
+    label: 'Week 2 Notes',
+    url: 'notes2.html',
+  },
+  {
+    label: 'Week 3 Notes',
+    url: 'notes3.html',
+  },
+  {
+    label: 'Week 4 Notes',
+    url: 'notes4.html',
+  },
+  {
+    label: 'Week 5 Notes',
+    url: 'week5.html',
+  },
+  {
+    label: 'Week 6 Notes',
+    url: 'todo_list.html',
+  },
+  {
+    label: 'Week 7 Notes',
+    url: 'week7.html',
+  },
+];
+
+
+
+const ol = document.getElementById('assignments'); // whatever your OL tag ID is
+
+links.forEach(link => {
+
+  let li = document.createElement('li');
+
+  let a = document.createElement('a');
+
+  a.setAttribute('href', link.url);
+
+  a.innerText = link.label;
+
+  // add other tags of interest if you like (date submitted, score)
+
+  li.appendChild(a);
+
+  ol.appendChild(li);
+
+});
