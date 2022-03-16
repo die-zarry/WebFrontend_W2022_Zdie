8a1696f6d4c42cecfd3a88103e34a1cf6d359fc4
Zarry <zarrydie03@gmail.com>
2022-03-16 04:24:13 -0400

    updated index page

---
+++ week9/bmi.js
@@ -0,0 +1,59 @@
+window.onload = () => {
+	let button = document.querySelector("#btn");
+
+	// Function to calculate BMI
+	button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+
+	/* Getting input from user and store it to height variable after converting it to an int. */
+	let height = parseInt(document
+			.querySelector("#height").value);
+
+	/* Getting input from user and store it into weight varaible after converting it to an int.*/
+	let weight = parseInt(document
+			.querySelector("#weight").value);
+
+	let output = document.querySelector("#output");
+
+	// Checking the user providing a proper
+	// value or not
+	if (height === "" || isNaN(height))
+		output.innerHTML = "Please ENTER a valid Height!";
+
+	else if (weight === "" || isNaN(weight))
+		output.innerHTML = "Please ENTER a valid Weight!";
+
+	// If both input is valid, calculate the bmi
+	else {
+
+		// Fixing upto 2 decimal places
+		let bmi = (weight / ((height * height)
+							/ 10000)).toFixed(3);
+
+            /* Your BMI Categories:
+            Underweight = <18.5
+            Normal weight = 18.5–24.9
+            Overweight = 25–29.9
+            Obesity = BMI of 30 or greater*/ 
+                  
+		// Dividing as per the bmi conditions
+		if (bmi < 18.6) output.innerHTML =
+			`You are Under Weight : <span>${bmi}</span>`;
+
+		else if (bmi >= 18.6 && bmi < 24.9)
+			output.innerHTML =
+			`You are Normal : <span>${bmi}</span>`;
+        else if (bmi >= 25 && bmi < 29.9)
+			output.innerHTML =
+			`You are Overweight : <span>${bmi}</span>`; 
+
+		else output.innerHTML =
+			`You are Obese : <span>${bmi}</span>`;
+
+
+
+
+	}
+}
